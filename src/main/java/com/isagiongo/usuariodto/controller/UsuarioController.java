6466728ac605ce3bd184b87bb0a24fd22d1905d7
Isa Giongo <isagiongo@gmail.com>
2019-02-22 21:19:19 -0300

    Adiciona tratamento de excecoes e metodo find

--- src/main/java/com/isagiongo/usuariodto/controller/UsuarioController.java
+++ src/main/java/com/isagiongo/usuariodto/controller/UsuarioController.java
@@ -1,11 +1,18 @@
 package com.isagiongo.usuariodto.controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isagiongo.usuariodto.dto.UsuarioDTO;
@@ -22,8 +29,14 @@ public class UsuarioController {
 	private UsuarioService usuarioService;
 
 	@PostMapping
-	public ResponseEntity<UsuarioRespostaDTO> salvar(@RequestBody UsuarioDTO usuarioDTO){
+	public ResponseEntity<UsuarioRespostaDTO> salvar(@RequestBody @Valid UsuarioDTO usuarioDTO){
 		Usuario usuario = usuarioService.salvar(usuarioDTO.transformaParaObjeto());
 		return new ResponseEntity<>(UsuarioRespostaDTO.transformaEmDTO(usuario), HttpStatus.CREATED);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<Usuario>> buscarPorId(@PathVariable Long id){
+		Optional<Usuario> obj = usuarioService.buscarPorId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/isagiongo/usuariodto/dto/UsuarioDTO.java
+++ src/main/java/com/isagiongo/usuariodto/dto/UsuarioDTO.java
@@ -1,5 +1,8 @@
 package com.isagiongo.usuariodto.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.isagiongo.usuariodto.model.Usuario;
 
 import lombok.Getter;
@@ -7,8 +10,13 @@ import lombok.Getter;
 @Getter
 public class UsuarioDTO {
 
+	@NotBlank(message = "{nome.not.blank}")
 	private String nome;
+	
+	@NotBlank(message = "{email.not.blank}") @Email(message = "{email.not.valid}")
     private String email;
+	
+	@NotBlank(message = "{senha.not.blank}")
     private String senha;
     
 	public Usuario transformaParaObjeto() {
